07c033b8f6fb812f31e02ee78e98ebb52ae9307c
tucchun <tucchun@hotmail.com>
2017-11-06 18:14:57 +0800

    todo

---
+++ todos/src/containers/App.js
@@ -0,0 +1,45 @@
+import React, {Component, PropTypes} from 'react';
+import {connect} from 'react-redux';
+import {addToDo, completeTodo, setVisibilityFilter, VisibilityFilters} from '../actions/actions';
+import AddTodo from '../components/AddTodo';
+import TodoList from '../components/TodoList';
+import Footer from '../components/footer';
+
+class App extends Component {
+  render() {
+    const {dispatch, visibleTodos, visibilityFilter} = this.props;
+
+    return (
+      <div>
+        <AddTodo onAddClick={text => dispatch(addToDo(text))}/>
+        <TodoList todos={visibleTodos} onTodoClick= {index => dispatch(completeTodo(index))}/>
+        <Footer filter={visibilityFilter} onFilterChange= {nextFilter => dispatch(setVisibilityFilter(nextFilter))}/>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  visibleTodos: PropTypes.arrayOf(PropTypes.shape({text: PropTypes.string.isRequired, completed: PropTypes.bool.isRequired}).isRequired).isRequired,
+  visibilityFilter: PropTypes.oneOf(['SHOW_ALL', 'SHOW_COMPLETED', 'SHOW_ACTIVE']).isRequired
+}
+
+function selectTodos(todos, filter) {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos;
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed);
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed)
+  }
+}
+
+function select(state) {
+  return {
+    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
+    visibilityFilter: state.visibilityFilter
+  }
+}
+
+export default connect(select)(App);
